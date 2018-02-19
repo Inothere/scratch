e1e56aa8e7dbee04304bc6adb8f437f99e04dd31
chendi <chendi@Ctrip.com>
2018-02-19 13:58:00 +0800

    change file name

---
+++ loveviolet.py
@@ -0,0 +1,23 @@
+print 'Hello world'
+def fib(n):
+    i = 0
+    a = 0
+    b = 1
+    if n == 0:
+        return 0
+    for i in range(n):
+        c = b
+        b = a+b
+        a = cwhi
+    return b
+
+def fib2(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib2(n - 1) + fib2(n - 2)
+
+if __name__ == '__main__':
+    for i in range(5):
+        print fib(i)
